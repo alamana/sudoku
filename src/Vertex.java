211c0a7200eea6b1b4207afe21a7975973154c70
alamana <alex.lamana92@gmail.com>
2013-04-17 10:33:57 -0400

    vertex

---
+++ src/Vertex.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Vertex {
+	
+	public int value;
+	public ArrayList<Vertex> neighbours;
+	
+	public Vertex(){
+		neighbours = new ArrayList<Vertex>();
+		value = 0;
+	}
+	
+	
+}
